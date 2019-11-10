9d251a3ccb21708210c75dee8d6038ee6343eefc
Mike Freeman <michael.k.freeman@gmail.com>
2019-11-10 12:18:59 +0100

    Updates

---
+++ precache-manifest.b7b9bc0766f74d9b00a2e9413867f254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19df02c98c71460db8d8",
+    "url": "/uw-physics/static/js/runtime~main.19df02c9.js"
+  },
+  {
+    "revision": "ce64f6d33c52dfe3ea87",
+    "url": "/uw-physics/static/js/main.ce64f6d3.chunk.js"
+  },
+  {
+    "revision": "438c69a930e9876c0771",
+    "url": "/uw-physics/static/js/1.438c69a9.chunk.js"
+  },
+  {
+    "revision": "ce64f6d33c52dfe3ea87",
+    "url": "/uw-physics/static/css/main.f6b8c21d.chunk.css"
+  },
+  {
+    "revision": "96a0ebda0d21d9bb70962ae292759b15",
+    "url": "/uw-physics/index.html"
+  }
+];
